a11c44dc190dfeb236f355a7004198c9e02977fe
FCVEER <rik@veerhoek.nl>
2022-08-18 16:22:23 +0200

    add

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,9 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  server: {
+    port: 3001
+  },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
